e44521f9e32b3c726dc1ccf8156850728f649a4e
renbin <renbin@chebada.com>
2019-06-18 17:11:09 +0800

    初步尝试美团方案成功

--- app_plugin/src/main/java/com/warm/app_plugin/MainActivity.java
+++ app_plugin/src/main/java/com/warm/app_plugin/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
--- buildsrc/src/main/groovy/com/warm/Inject.groovy
+++ buildsrc/src/main/groovy/com/warm/Inject.groovy
@@ -4,6 +4,11 @@ import com.google.common.io.Files
 import javassist.ClassPool
 import javassist.CtClass
 import org.apache.commons.io.FileUtils
+import org.objectweb.asm.ClassReader
+import sun.tools.jar.resources.jar
+
+import java.util.jar.JarFile
+import java.util.zip.ZipFile
 
 
 class Inject {
@@ -46,6 +51,30 @@ class Inject {
         println "------------------"
         println("absolutePath=${absolutePath},dest=${dest}")
         println "------------------"
+        Files.createParentDirs(new File(dest))
+
+        def jarFile = new JarFile(absolutePath)
+
+        def enumeration = jarFile.entries();
+
+        while (enumeration.hasMoreElements()) {
+            def jarEntry = enumeration.nextElement()
+            def entryName = jarEntry.name
+            if (!jarEntry.isDirectory() && entryName.endsWith(".class") && !entryName.contains('R$') && !entryName.contains('R.class') && !entryName.contains('$') && !entryName.contains('BuildConfig.class')) {
+                ClassReader reader = new ClassReader(jarFile.getInputStream(jarEntry));
+                if (reader.superName == "android/view/View"){
+                    def itemViewCtClass = pool.get(reader.className)
+                    if (itemViewCtClass.isFrozen()) {
+                        itemViewCtClass.defrost()
+                    }
+                    itemViewCtClass.superclass = pool.get("com.warm.app_plugin.MyView")
+                    itemViewCtClass.writeFile()
+                }else {
+
+                }
+            }
+
+        }
 
         FileUtils.copyFile(new File(absolutePath), new File(dest))
 
